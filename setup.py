ce3a1489abf45286ef334172af4d40660376df6d
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2010-02-13 13:44:09 +0100

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='pytest-codecheckers',
     description='pytest addon to add code-checking as source for testcases',
-    version='0.0',
+    version='0.1',
     author='Ronny Pfannschmidt',
     author_email='Ronny.Pfannschmidt@gmx.de',
     packages=[
