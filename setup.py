20f42b3fb71699c611d55237d2b21ec9289f29a2
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2012-06-20 23:26:38 +0200

    add tox envs, fixes

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ py.test plugin for checking PEP8 source code compliance using pyflakes.
 Usage
 ---------
 
-after installation (e.g. via ``pip install pytest-codecheckers``) you can type::
+after installation (e.g. via ``pip install pytest-codecheckers``)
+you can type::
 
     py.test [path/to/mypkg]
 
@@ -14,10 +15,9 @@ further questions please send them to the `pytest-dev`_ mailing list.
 .. _`pytest-dev`: http://codespeak.net/mailman/listinfo/py-dev
 """
 from setuptools import setup
-from hgdistver import get_version
 setup(
     name='pytest-codecheckers',
-    description='pytest plugin to add source code sanity checks (pep8 and friends)',
+    description='pytest plugin to add source code sanity checks',
     long_description=__doc__,
     get_version_from_scm=True,
     author='Ronny Pfannschmidt',
@@ -25,20 +25,20 @@ setup(
     url='http://bitbucket.org/RonnyPfannschmidt/pytest-codecheckers/',
     packages=[
         'codecheckers',
-        ],
+    ],
     entry_points={
         'pytest11': [
             'codechecker = codecheckers.plugin',
-            ],
+        ],
         'codechecker': [
             'pep8 = codecheckers.pep',
             'pyflakes = codecheckers.flakes',
-            ],
-        },
+        ],
+    },
     install_requires=[
         'pytest>=2.0',
         'pyflakes>=0.4',
         'pep8',
-        ],
+    ],
     setup_requires=['hgdistver'],
-    )
+)
