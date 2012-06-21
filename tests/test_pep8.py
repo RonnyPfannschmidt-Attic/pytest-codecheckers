cf9871159a8dd178473f9d4a5e7b4e1cab74eb0e
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2012-06-21 14:56:10 +0200

    tests for pep8

---
+++ tests/test_pep8.py
@@ -0,0 +1,34 @@
+def pytest_funcarg__testdir(request):
+    testdir = request.getfuncargvalue('testdir')
+    testdir.makeini('[pytest]\ncodechecks = pep8')
+    return testdir
+
+
+def test_badcode(testdir):
+    testdir.makepyfile('''
+        def a():
+            pass
+        def b():
+            pass''')
+    out = testdir.runpytest('--tb=short', '-v')
+    out.stdout.fnmatch_lines([
+        '*lines*',
+        '*1 failed*',
+    ])
+
+
+def test_goodcode(testdir):
+    p = testdir.makepyfile('''
+        def a():
+            pass
+
+
+        def b():
+            pass
+
+        ''')
+    p.write(p.read() + '\n')
+    out = testdir.runpytest('--tb=short', '-v')
+    out.stdout.fnmatch_lines([
+        '*1 passed*',
+    ])
